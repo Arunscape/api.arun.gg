0c2e8e3c89020c856f60046803306db9cf5156fe
Arunscape <8227297+Arunscape@users.noreply.github.com>
2024-08-15 01:04:20 -0600

    add random colour endpoint

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use {
     },
     rand::prelude::*,
     serde::{Deserialize, Serialize},
+    serde_json::json,
     std::sync::LazyLock,
     tracing_subscriber::prelude::*,
 };
@@ -39,7 +40,8 @@ async fn main() -> Result<(), anyhow::Error> {
     let app = Router::new()
         .route("/", get(root))
         .route("/coin", get(flip_a_coin))
-        .route("/random_number", get(random_number));
+        .route("/random_number", get(random_number))
+        .route("/random_colour", get(random_colour));
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", *PORT)).await?;
     tracing::info!("Listening on {:?}", listener.local_addr()?);
@@ -68,4 +70,25 @@ async fn flip_a_coin() -> &'static str {
 async fn random_number() -> impl IntoResponse {
     let mut rng = rand::thread_rng();
     rng.gen::<u128>().to_string()
+}
+
+async fn random_colour() -> impl IntoResponse {
+    let mut rng = rand::thread_rng();
+
+    let r: u8 = rng.gen();
+    let g: u8 = rng.gen();
+    let b: u8 = rng.gen();
+    let a: u8 = rng.gen();
+    let a_div = (a as f32 / 255.0 * 100.0).round() / 100.0;
+
+    let rgba_str = format!("rgba({}, {}, {}, {})", r, g, b, a_div);
+    let hex = format!("#{:02x}{:02x}{:02x}{:02x}", r, g, b, a);
+
+    let res = json!({
+        "rgba": {"r": r, "g": g, "b": b, "a": a_div.to_string()},
+        "rgba_str": rgba_str,
+        "hex": hex,
+    });
+
+    Json(res)
 }
