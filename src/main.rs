3dc8a376feb19e19676921646a46f28b1e9ae7af
Arunscape <8227297+Arunscape@users.noreply.github.com>
2025-05-15 00:05:53 -0600

    add celsius farenheit unit converter because I was bored but I didn't have that much time to fill lol

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 use {
-    axum::{
-        http::StatusCode,
-        response::IntoResponse,
-        routing::{get, post},
-        Json, Router,
-    },
+    axum::{Json, Router, extract::Path, response::IntoResponse, routing::get},
     rand::prelude::*,
-    serde::{Deserialize, Serialize},
     serde_json::json,
     std::sync::LazyLock,
     tracing_subscriber::prelude::*,
@@ -41,7 +35,9 @@ async fn main() -> Result<(), anyhow::Error> {
         .route("/", get(root))
         .route("/coin", get(flip_a_coin))
         .route("/random_number", get(random_number))
-        .route("/random_colour", get(random_colour));
+        .route("/random_colour", get(random_colour))
+        .route("/ctof/:n", get(celsius_to_farenheit))
+        .route("/ftoc/:n", get(farenheit_to_celsius));
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", *PORT)).await?;
     tracing::info!("Listening on {:?}", listener.local_addr()?);
@@ -54,35 +50,31 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn flip_a_coin() -> &'static str {
+async fn flip_a_coin() -> impl IntoResponse {
     let coin = {
-        let mut rng = rand::thread_rng();
-        rng.gen::<bool>()
+        let mut rng = rand::rng();
+        rng.random::<bool>()
     };
 
-    if coin {
-        "heads"
-    } else {
-        "tails"
-    }
+    if coin { "heads" } else { "tails" }
 }
 
 async fn random_number() -> impl IntoResponse {
-    let mut rng = rand::thread_rng();
-    rng.gen::<u128>().to_string()
+    let mut rng = rand::rng();
+    rng.random::<u128>().to_string()
 }
 
 async fn random_colour() -> impl IntoResponse {
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
 
-    let r: u8 = rng.gen();
-    let g: u8 = rng.gen();
-    let b: u8 = rng.gen();
-    let a: u8 = rng.gen();
+    let r: u8 = rng.random();
+    let g: u8 = rng.random();
+    let b: u8 = rng.random();
+    let a: u8 = rng.random();
     let a_div = (a as f32 / 255.0 * 100.0).round() / 100.0;
 
-    let rgba_str = format!("rgba({}, {}, {}, {})", r, g, b, a_div);
-    let hex = format!("#{:02x}{:02x}{:02x}{:02x}", r, g, b, a);
+    let rgba_str = format!("rgba({r}, {g}, {b}, {a_div})");
+    let hex = format!("#{r:02x}{g:02x}{b:02x}{a:02x}");
 
     let res = json!({
         "rgba": {"r": r, "g": g, "b": b, "a": a_div.to_string()},
@@ -91,4 +83,14 @@ async fn random_colour() -> impl IntoResponse {
     });
 
     Json(res)
+}
+
+async fn celsius_to_farenheit(Path(num): Path<f64>) -> impl IntoResponse {
+    let n = 1.8 * num + 32.0;
+    n.to_string()
+}
+
+async fn farenheit_to_celsius(Path(num): Path<f64>) -> impl IntoResponse {
+    let n = 5.0 / 9.0 * (num - 32.0);
+    n.to_string()
 }
